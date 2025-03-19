85141b7b7eb464fb19934a79df56d74efb694911
Vladimir Zaets <vzaets98@mail.ru>
2025-03-19 10:47:06 +0700

    go back :)

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-# just testing repository rename
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
